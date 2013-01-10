eb04a04d6dab5081514c27b39555e9c96e7caeea
Ivan Porto Carrero <ivan@flanders.co.nz>
2013-01-10 12:30:42 +0100

    use 2.9.2 version of specs for all 2.9 builds

--- project/build.scala
+++ project/build.scala
@@ -69,8 +69,8 @@ object RlSettings {
         "-Xcheckinit",
         "-encoding", "utf8"),
       libraryDependencies <+= (scalaVersion) {
-        case "2.9.0-1" => "org.specs2" %% "specs2" % "1.5" % "test"
-        case "2.9.0" => "org.specs2" % "specs2_2.9.0-1" % "1.5" % "test"
+        // case "2.9.0-1" |  => "org.specs2" % "specs2_2.9.2" % "1.12" % "test"
+        // case "2.9.0" => "org.specs2" % "specs2_2.9.0-1" % "1.5" % "test"
         case v if v.startsWith("2.9") => "org.specs2" %% "specs2" % "1.12" % "test"
         case _ => "org.specs2" %% "specs2" % "1.13" % "test"
       },
